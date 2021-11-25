ca1fc990a7865db7453a3fec0004f6ec9bcf0a7b
Administrador <Administrador@ERNESTO2021>
2021-11-25 01:12:32 -0400

    encabezado

--- PaginaWebTravel/Principal.Master.cs
+++ PaginaWebTravel/Principal.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,28 @@ namespace PaginaWebTravel
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Clear();
+                dt.Columns.Add("DESCRIPCION");
+                dt.Columns.Add("DETALLE");
+                foreach (DataRow dr in Clases.Encabezados.GET_PAG_ENCABEZADO("A").Rows)
+                {
+                    if (dr["DESC_TIPO"].ToString() == "SECCION")
+                    {
+                        DataRow dr1 = dt.NewRow();
+                        dr1["DESCRIPCION"] = dr["DESCRIPCION"];
+                        dr1["DETALLE"] = dr["DESCRIPCION"].ToString()+".ASPX";
+                        dt.Rows.Add(dr1);
+                    }
+                    
 
+                }
+                Repeater1.DataSource = dt;
+                Repeater1.DataBind();
+
+            }
         }
     }
 }
--- PaginaWebTravel/Principal.Master.designer.cs
+++ PaginaWebTravel/Principal.Master.designer.cs
@@ -33,6 +33,15 @@ namespace PaginaWebTravel
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control Repeater1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater1;
+
+        /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
         /// <remarks>
